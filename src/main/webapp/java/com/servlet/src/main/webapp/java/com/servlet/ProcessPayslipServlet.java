37c89d1bbeee3eacce260daa2f7f420d83b9e73d
Shangeri Sivalingam <anushangeri@gmail.com>
2019-04-29 19:23:52 +0800

    added servlet

--- src/main/webapp/java/com/servlet/src/main/webapp/java/com/servlet/ProcessPayslipServlet.java
+++ src/main/webapp/java/com/servlet/src/main/webapp/java/com/servlet/ProcessPayslipServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ProcessPayslipServlet
  */
-@WebServlet("/ProcessPayslipServlet")
+@WebServlet(name="ProcessPayslipServlet",urlPatterns="/ProcessPayslipServlet") 
 public class ProcessPayslipServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
